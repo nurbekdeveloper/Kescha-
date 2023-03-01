62ae7242b3c13e6a5d92ad3a45cc3ecd5985aa01
nurbekdeveloper <nurbeknormatov02@gmail.com>
2023-03-01 03:06:30 -0800

    add age deffrence

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("Enter your full name ");
+int keshaAge = 5;
+string name = Console.ReadLine();
+System.Console.WriteLine("Enter your age ");
+int yourAge = Convert.ToInt32(Console.ReadLine());
+int result = yourAge -keshaAge ;
+System.Console.WriteLine($"Sizning yoshingiz keshaning yoshidan {result } ga katta ");
+
+

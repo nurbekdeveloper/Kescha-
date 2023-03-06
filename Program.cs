88a1e49b1de5c9ec5e0224ab48c3a16ccee9992e
nurbekdeveloper <nurbeknormatov02@gmail.com>
2023-03-06 03:20:47 -0800

    added do while this project

--- Program.cs
+++ Program.cs
@@ -1,9 +1,72 @@
-﻿System.Console.WriteLine("Enter your name ");
-int keshaAge = - 3;
+﻿
+string password = "";
+do{
+    System.Console.WriteLine("enter your password for Kescha ");
+    password = Console.ReadLine();
+
+}
+while (password != "password");
+
+string[] random  = {"demo2123" ,"salom111","muborak2323"};
+System.Console.WriteLine(random[2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+System.Console.WriteLine("Enter your name ");
+int keshaAge = 3;
 string name = Console.ReadLine();
 System.Console.WriteLine("Enter your age ");
+
 int yourAge = Convert.ToInt32(Console.ReadLine());
 int result = yourAge - keshaAge ;
+System.Console.WriteLine("you are operation  is ");
+string operation = Console.ReadLine();
 
 
 
@@ -28,3 +91,15 @@ string fristresult =
             ? "1 nd is positive  "
             :" 1nd  is negative " ;
             System.Console.WriteLine(secondresult);           
+
+
+string switchResult  = operation switch  {
+    "+" => $"{yourAge} + {keshaAge} = {yourAge + keshaAge} " ,
+    "-" => $"{yourAge} - {keshaAge} = {yourAge - keshaAge}" ,
+    "/" => $"{yourAge} / {keshaAge} = {yourAge / keshaAge} " ,
+    "*" => $"{yourAge} * {keshaAge} =  {yourAge * keshaAge} " ,
+    "%" => $"{yourAge} % {keshaAge} =  {yourAge % keshaAge} " ,
+    _   => "Operation is not found !"
+};
+System.Console.WriteLine(switchResult);
+
